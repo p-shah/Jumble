25424d376e3eae37408587d5985a453796468bfe
tdinh7 <tdinh7@villanova.edu>
2017-11-05 19:20:55 -0500

    modal
    Added a modal to the UI which appears when the game is beaten... all data is reset when a new game begins

--- Jumble/Jumble/Controllers/HomeController.cs
+++ Jumble/Jumble/Controllers/HomeController.cs
@@ -31,48 +31,67 @@ namespace Jumble.Controllers
         [HttpPost]
         public JsonResult GetAllDataFromCsvFile()
         {
-            var dataFile = Server.MapPath("~/Files/riddle.csv");
-            List<Data> result = new List<Data>();
-            if (System.IO.File.Exists(dataFile))
+            var dataFile = Server.MapPath("~/Files/riddle.csv");//where the riddles are hard coded at
+
+            List<Data> result = new List<Data>();//this is a collection of Data objects
+
+            if (System.IO.File.Exists(dataFile))//fires if the file exists
             {
-             var  userData = System.IO.File.ReadAllLines(dataFile);
-                if (userData != null)
+                var userData = System.IO.File.ReadAllLines(dataFile);
+                if (userData != null)//fires if the file has content inside
                 {
                     for (int i = 0; i < userData.Length; i++)
                     {
-                        var line = userData[i];
-                        Data d = new Data();
+                        var line = userData[i];//this is each line or each riddle
+
+                        Data dataOBJ = new Data();
+                        //public string Riddle { get; set; }
+                        //public Tuple<string, string> JumbleWord1 { get; set; }
+                        //public Tuple<string, string> JumbleWord2 { get; set; }
+                        //public Tuple<string, string> JumbleWord3 { get; set; }
+                        //public Tuple<string, string> JumbleWord4 { get; set; }
+                        //public string Answer { get; set; }
+
+                        //this is a collection of Strings
                         var values = line.Split(',');
+                        //if ther are no " characters, the first index will be the riddle and all of the preceeding indexes will be the answers to the word jumbles and 
+                        //the last index will be the answer to the riddle
+
                         var currentIndex = 0;
-                        if (values[currentIndex].Contains("\""))
-                        {
+
+                        if (values[currentIndex].Contains("\""))//fires for lines or riddles that are surrounded by " "
+                        {//the first index will have a " character before the first half of the riddle
                             var myQuestion = "";
 
-                            myQuestion += values[currentIndex]+",";
+                            myQuestion += values[currentIndex] + ",";
                             currentIndex++;
+
                             while (!values[currentIndex].Contains("\""))
                             {
-                                myQuestion += values[currentIndex]+",";
+                                myQuestion += values[currentIndex] + ",";
                                 currentIndex++;
                             }
-                            myQuestion += values[currentIndex];
-                            d.Riddle = myQuestion.Replace("\"", String.Empty);
-                        }
-                        else
-                        {
-                            d.Riddle = values[currentIndex];
-                        }
-                        d.JumbleWord1 = GetWordWithHint(values[currentIndex+1]);
-                        d.JumbleWord2 = GetWordWithHint(values[currentIndex + 2]);
-                        d.JumbleWord3 = GetWordWithHint(values[currentIndex + 3]);
-                        d.JumbleWord4 = GetWordWithHint(values[currentIndex + 4]);
 
-                        d.Answer = values[currentIndex+5];
-                        result.Add(d);
+                            myQuestion += values[currentIndex];
+                            dataOBJ.Riddle = myQuestion.Replace("\"", String.Empty);//replaces the " character with an empty String
+                        }
+                        else//fires for lines or riddles that are not surrounded by " "
+                        {
+                            dataOBJ.Riddle = values[currentIndex];
+                        }
+
+                        dataOBJ.JumbleWord1 = GetWordWithHint(values[currentIndex + 1]);
+                        dataOBJ.JumbleWord2 = GetWordWithHint(values[currentIndex + 2]);
+                        dataOBJ.JumbleWord3 = GetWordWithHint(values[currentIndex + 3]);
+                        dataOBJ.JumbleWord4 = GetWordWithHint(values[currentIndex + 4]);
+
+                        dataOBJ.Answer = values[currentIndex + 5];
+
+                        result.Add(dataOBJ);
                     }
                 }
             }
-            return Json(result,JsonRequestBehavior.AllowGet);
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
 
         private Tuple<string, string> GetWordWithHint(string word)
@@ -88,6 +107,6 @@ namespace Jumble.Controllers
             }
             return new Tuple<string, string>(word.ToLower(), hint);
         }
-     
+
     }
 }
--- Jumble/Jumble/Scripts/Home/index.js
+++ Jumble/Jumble/Scripts/Home/index.js
@@ -1,16 +1,21 @@
 ﻿$(document).ready(function () {
 
     loadAllDataFromCSV();
+    //fires the "GetAllDataFromCsvFile" method in the HomeControllers, which returns a collection of "Data objects" and sets the 'allDataFromCSV' variable
+    //each Data object represents each riddle or line
+
+
     var counter = 0;
     var totalQuestions = 0;
     var totalCorrectAnswer = 0;
     var time = '';
-    var allDataFromCSV = null;
+
+    var allDataFromCSV = null; // the "loadAllDataFromCSV()" methods sets this variable 
+    //a collection of "Data" objects
+
     var currentDataIndex = 0;
     var startEndBtnText = 'Start Game7';
     var imageCounter = 1;
-
-
 
     $('#gameDiv').hide();
 
@@ -41,7 +46,6 @@
         $('#themePicture').show();
         //shows the theme image
 
-
         counter = 0;
         $('#counter').show();
         $('#tdTotalQuestions').text(totalQuestions);
@@ -49,9 +53,9 @@
 
         var btnText = $('#btnStartAndEnd').text();
 
-        if (btnText == startEndBtnText) {
+        if (btnText == 'Start Game7') {//fires when the "Start Game" button is pushed
 
-            alert('solve all riddles within 20 minutes');
+            alert('Solve All Riddles Within 20 Minutes To Be A Boss!');
 
             document.getElementById("readyToPlayP").style.visibility = "hidden";//hides the p tag with the ready to paly message
             document.getElementById("btnCheck").style.visibility = "visible";//shows 'check answer' button
@@ -70,6 +74,7 @@
             $('#tdJumbleWord4').text(shuffle(allDataFromCSV[currentDataIndex].JumbleWord4.Item1));
         }
         else {//this will fire every time except the first time around
+            //fires when the "Give Up" button is pushed
             document.getElementById("readyToPlayP").style.visibility = "visible";//shows the p tag with the ready to paly message
             document.getElementById("btnCheck").style.visibility = "hidden";//hides 'check answer' button
 
@@ -87,9 +92,7 @@
             $('#counter').hide();
             $('#tdTotalCorrectAnswers').text(totalCorrectAnswer);
             $('#totalTimeTaken').text('');
-
         }
-
     });
 
     $('#btnCheck').on('click', function () {
@@ -176,30 +179,94 @@
                 showGrade();
             }
 
-            //code to check the score and decide if the game was beaten or not
-            if (totalCorrectAnswer == 3) {
-                alert("You've Beaten This Game!")
-                //loadAllDataFromCSV();
+            //code to check the score and decide if the game is beaten or not
+            if (totalCorrectAnswer == 4) {
+                //code here to: 
+                //1:hide the theme picuture 
+                $('#themePicture').hide();
 
-                //var counter = 0;
-                //var totalQuestions = 0;
-                //var totalCorrectAnswer = 0;
-                //var time = '';
-                //var allDataFromCSV = null;
-                //var currentDataIndex = 0;
 
-                //document.getElementById("readyToPlayP").style.visibility = "visible";//hides the p tag with the ready to paly message
-                //var btnText = $('#btnStartAndEnd').text('Start Game7');//changes the texts
+                // Get the modal
+                var modal = document.getElementById('myModal');
 
-                //totalCorrectAnswer = 0;
-                //$('#gameDiv').show();
-                //$('.clsAnswer').val('');
-                //$('#txtFinalAnswer').val('');
-                //$('#tdRiddle').text(allDataFromCSV[currentDataIndex].Riddle);
-                //$('#tdJumbleWord1').text(shuffle(allDataFromCSV[currentDataIndex].JumbleWord1.Item1));
-                //$('#tdJumbleWord2').text(shuffle(allDataFromCSV[currentDataIndex].JumbleWord2.Item1));
-                //$('#tdJumbleWord3').text(shuffle(allDataFromCSV[currentDataIndex].JumbleWord3.Item1));
-                //$('#tdJumbleWord4').text(shuffle(allDataFromCSV[currentDataIndex].JumbleWord4.Item1));
+                // Get the <span> element that closes the modal
+                var span = document.getElementsByClassName("close")[0];
+
+                modal.style.display = "block";//displays the modal
+
+                // When the user clicks on <span> (x), close the modal
+                span.onclick = function () {
+                    modal.style.display = "none";
+                    //----------code to reset the game----------
+                    //1:reset values: counter,totalQuestions,totalCorrectAnswer, time, allDataFromCSV, currentDataIndex
+                    //2: hide the "Check Answer" button
+                    //3:show the 'readyToPlayP' paragraph tag
+                    //4: set the 'btnStartAndEnd' button to "Start Game""
+                    //5: set the 'imageCounter' variable to 1
+
+                    document.getElementById("readyToPlayP").style.visibility = "visible";//hides the p tag with the ready to paly message
+                    document.getElementById("btnCheck").style.visibility = "hidden";//hides 'check answer' button
+                    $('#gameDiv').hide();
+                    startEndBtnText = $('#btnStartAndEnd').text('Start Game7');//changes the texts of the "btnStartAndEnd" button
+                    $("#themePicture").attr("src", "/Pictures/theme1.png");//resets the theme pictures
+
+                    //code to manually clear the feilds for the 'Total Correct Answers' and 'Time Taken Since Last Correct Answer' since the updated changes won't show until the page refreshes
+                    $('#tdTotalCorrectAnswers').text("");
+                    $('#totalTimeTaken').text("");
+                    //code to manually clear the feilds for the 'Total Correct Answers' and 'Time Taken Since Last Correct Answer' since the updated changes won't show until the page refreshes
+
+
+                    allDataFromCSV = null; //clears the collection of "Data" objects
+                    loadAllDataFromCSV();//sets the the 'allDataFromCSV' variable to a collection of "Data" objects
+                    counter = 0;
+                    totalQuestions = 0;
+                    totalCorrectAnswer = 0;
+                    time = '';//resets timer
+                    allDataFromCSV = null;
+                    currentDataIndex = 0;
+                    imageCounter = 1;
+                    alert('inside 1' + '\n' + totalCorrectAnswer.toString());
+                    //----------code to reset the game----------
+                }
+
+                // When the user clicks anywhere outside of the modal, close it
+                window.onclick = function (event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+
+                        //----------code to reset the game----------
+                        //1:reset values: counter,totalQuestions,totalCorrectAnswer, time, allDataFromCSV, currentDataIndex
+                        //2:
+                        //3:show the 'readyToPlayP' paragraph tag
+                        //4: set the 'btnStartAndEnd' button to "Start Game""
+                        //5: set the 'imageCounter' variable to 1
+
+                        document.getElementById("readyToPlayP").style.visibility = "visible";//hides the p tag with the ready to paly message
+                        document.getElementById("btnCheck").style.visibility = "hidden";//hides 'check answer' button
+                        $('#gameDiv').hide();
+                        startEndBtnText = $('#btnStartAndEnd').text('Start Game7');//changes the texts of the "btnStartAndEnd" button
+                        $("#themePicture").attr("src", "/Pictures/theme1.png");//resets the theme pictures
+
+                        //code to manually clear the feilds for the 'Total Correct Answers' and 'Time Taken Since Last Correct Answer' since the updated changes won't show until the page refreshes
+                        $('#tdTotalCorrectAnswers').text("");
+                        $('#totalTimeTaken').text("");
+                        //code to manually clear the feilds for the 'Total Correct Answers' and 'Time Taken Since Last Correct Answer' since the updated changes won't show until the page refreshes
+
+                        //allDataFromCSV = null; //clears the collection of "Data" objects
+                        loadAllDataFromCSV();//sets the the 'allDataFromCSV' variable to a collection of "Data" objects
+                        counter = 0;
+                        totalQuestions = 0;
+                        totalCorrectAnswer = 0;
+                        time = '';//resets timer
+                        allDataFromCSV = null;
+                        currentDataIndex = 0;
+                        imageCounter = 1;
+                        alert('inside 2' + '\n' + totalCorrectAnswer.toString());
+                        //----------code to reset the game----------
+                    }
+                }
+
+
             }
         }
         else {
@@ -220,6 +287,7 @@
             $('#txtFirstWord').attr('readonly', 'true');
         }
     });
+
     $('#txtSecondWord').on('change', function () {
         if (allDataFromCSV[currentDataIndex].JumbleWord2.Item1.toLocaleLowerCase() == $('#txtSecondWord').val().toLocaleLowerCase()) {
             $('#tdHint2').text('Hint : ' + allDataFromCSV[currentDataIndex].JumbleWord2.Item2);
@@ -227,6 +295,7 @@
             $('#txtSecondWord').attr('readonly', 'true');
         }
     });
+
     $('#txtThirdWord').on('change', function () {
         if (allDataFromCSV[currentDataIndex].JumbleWord3.Item1.toLocaleLowerCase() == $('#txtThirdWord').val().toLocaleLowerCase()) {
             $('#tdHint3').text('Hint : ' + allDataFromCSV[currentDataIndex].JumbleWord3.Item2);
@@ -234,6 +303,7 @@
             $('#txtThirdWord').attr('readonly', 'true');
         }
     });
+
     $('#txtFourthWord').on('change', function () {
         if (allDataFromCSV[currentDataIndex].JumbleWord4.Item1.toLocaleLowerCase() == $('#txtFourthWord').val().toLocaleLowerCase()) {
             $('#tdHint4').text('Hint : ' + allDataFromCSV[currentDataIndex].JumbleWord4.Item2);
@@ -249,21 +319,23 @@
             totalQuestions = data.length;
         });
     }
+
     function showGrade() {
         //$('#counter').hide();//old
         $('#counter').show();//new
         var grade = (totalCorrectAnswer * 100) / allDataFromCSV.length;
         $('#grade').text(grade + "%");
-
-        // logic to display grade
-        //...........................
     }
+
     function shuffle(str) {
         a = [];
+
         for (var k = 0; k < str.length; k++) {
             a.push(str.charAt(k));
         }
+
         var j, x, i;
+
         for (i = a.length; i; i--) {
             j = Math.floor(Math.random() * i);
             x = a[i - 1];
@@ -272,11 +344,11 @@
         }
 
         var result = "";
+
         for (var l = 0; l < a.length; l++) {
             result += a[l];
         }
 
         return result;
-
     };
 });
